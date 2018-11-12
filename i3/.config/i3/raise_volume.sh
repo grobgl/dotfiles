53da85846d3955e1783207f3cb8fb0d90a8e70b2
Georg Grob <grobgl@gmail.com>
2018-11-12 20:35:22 +0000

    Redshift + more

--- i3/.config/i3/raise_volume.sh
+++ i3/.config/i3/raise_volume.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 x=$(pactl list sinks | grep '^[[:space:]]Volume:' | head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,' )
-if [[ $x -le 90 ]]
+if [[ $x -le 130 ]]
 then
     pactl set-sink-volume 0 +10%
 else
-    pactl set-sink-volume 0 100%
+    pactl set-sink-volume 0 140%
 fi
 
